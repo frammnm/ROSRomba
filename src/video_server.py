9a9a37584e144ba03efef6ff87bc961f3885875e
francisco martinez <frammnm@gmail.com>
2021-05-24 05:18:57 +0200

    Possible to receive image data from the robot camara and save it as jpeg.

--- src/video_server.py
+++ src/video_server.py
@@ -1,26 +1,48 @@
 #! /usr/bin/env python
 
 import rospy
-# from std_msgs.msg import String
-# from ex2_rendezvous.srv import gossip_update, gossip_updateResponse
-# from ex2_rendezvous.msg import queue_position_plot
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+import cv2
 import sys
 import random
+from matplotlib import pyplot
+import numpy as np
 
+
+bridge = CvBridge()
 
 class VideoServer():
   def __init__(self):
     self.video_topic_name = "/camera/rgb/image_raw/"
-    self.video_topic = rospy.Subscriber(self.video_topic_name, queue_position_plot, self.video_callback)
+    self.video_topic = rospy.Subscriber(self.video_topic_name, Image, self.video_callback)
+    self.rate = 1.0
+    self.img_count = 1
     self.start()
 
   def start(self):
     print("VideoServer started..")
+
+    while not rospy.is_shutdown():
+      pass 
+      
     return
 
 
-  def video_callback(self):
+  def video_callback(self, img):
     print("Received something..")
+    print("Image attributes:")
+    print("{ width: %d, height: %d, encoding: %s, is_bigendian: %d, step: %d}" % (img.width, img.height, img.encoding, img.is_bigendian, img.step))
+    # print(img)
+    try:
+        # Convert your ROS Image message to OpenCV2
+        cv2_img = bridge.imgmsg_to_cv2(img, "bgr8")
+    except CvBridgeError, e:
+        print(e)
+    else:
+        # Save your OpenCV2 image as a jpeg 
+        cv2.imwrite('/home/francisco/catkin_ws/src/robot_gui_bridge/tmp/%d.jpeg' % (self.img_count), cv2_img)
+        self.img_count += 1
     return
 
 
